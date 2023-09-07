014b622e19af3207afe9d6cc68fb77d3590d6cbf
zroesch <71677369+zroesch@users.noreply.github.com>
2023-09-07 16:03:37 -0700

    Starting Navigation

---
+++ app/src/main/java/com/example/yugiohcardscanner/navigation/NavigationComponents.kt
@@ -0,0 +1,15 @@
+package com.example.yugiohcardscanner.navigation
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun YugiohBottomNavigationBar(
+    NavigationBar{
+
+    }
+)
+
